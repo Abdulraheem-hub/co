aed817f6977e4906806a843ace1c7ad6835c9f31
AbdulRaheem Khan <abdulraheemzais@gmail.com>
2024-11-23 16:30:57 +0530

    add dcoker compose

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Copying custom files from repository to /var/www/html..."
+cp -rf /repo/* /var/www/html/
+
+echo "Fixing permissions..."
+chown -R www-data:www-data /var/www/html
+
+echo "Starting PrestaShop..."
+exec docker-php-entrypoint "$@"
