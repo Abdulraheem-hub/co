801b08e9fcf351685410c505b7f452b7e2229237
AbdulRaheem Khan <abdulraheemzais@gmail.com>
2024-11-25 16:49:16 +0530

    Change entrypoint for Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-set -e
-
-echo "Copying custom files from repository to /var/www/html..."
-cp -rf * /var/www/html/
-
-echo "Fixing permissions..."
-chown -R www-data:www-data /var/www/html
-
-echo "Starting PrestaShop..."
-exec docker-php-entrypoint "$@"
+Testing
